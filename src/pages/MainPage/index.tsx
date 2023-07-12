f788da0a21a0b80a245c96a0c4bfe749bdd27028
Andrew Pahomov <pahomov.kch@gmail.com>
2023-07-12 10:37:09 +0800

    Try native CSS nesting

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -6,12 +6,32 @@ const MainPage = memo(function MainPage() {
     <div className={styles.container}>
       <p>Hello APP</p>
 
+      <div className={styles.cq}>
+        <div className={styles.a}></div>
+        <div className={styles.b}></div>
+        <div className={styles.c}></div>
+      </div>
+
+
       <div className={styles.links}>
         <p className={styles.success}>success</p>
         <p className={styles.info}>info</p>
         <p className={styles.warning}>warning</p>
         <p className={styles.danger}>danger</p>
       </div>
+
+
+      <div style={{display: 'grid'}}>123</div>
+
+      <div className={styles['grid-container']}>
+        <span>Subgrids</span>
+        <span className={styles.a}></span>
+        <span className={styles.a}></span>
+        <span className={styles.a}></span>
+        <span className={styles.a}></span>
+
+      </div>
+
     </div>
   )
 })
