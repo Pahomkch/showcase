38d1a59ac5883b9750500f9f5d118f199907f7bc
Andrew Pahomov <pahomov.kch@gmail.com>
2023-07-26 14:08:29 +0800

    Install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,28 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
 
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:i18n-json/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:i18n-json/recommended', 'plugin:storybook/recommended'],
 
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-
-      files: ['.eslintrc.{js,cjs}'],
-
-      parserOptions: {
-        sourceType: 'script',
-      },
+  overrides: [{
+    env: {
+      node: true
     },
-  ],
+
+    files: ['.eslintrc.{js,cjs}'],
+
+    parserOptions: {
+      sourceType: 'script'
+    }
+  }],
 
   parser: '@typescript-eslint/parser',
 
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
 
   plugins: ['@typescript-eslint', 'react', 'i18next'],
@@ -45,21 +38,15 @@ module.exports = {
     'i18n-json/valid-json': 2,
     'i18n-json/identical-keys': 0,
 
-    'i18n-json/valid-message-syntax': [
-      2,
-      {
-        syntax: 'icu',
-      },
-    ],
+    'i18n-json/valid-message-syntax': [2, {
+      syntax: 'icu'
+    }],
 
-    'i18n-json/sorted-keys': [
-      2,
-      {
-        order: 'asc',
-        indentSpaces: 2,
-      },
-    ],
+    'i18n-json/sorted-keys': [2, {
+      order: 'asc',
+      indentSpaces: 2
+    }],
 
-    'i18next/no-literal-string': 2,
-  },
-}
+    'i18next/no-literal-string': 2
+  }
+};
